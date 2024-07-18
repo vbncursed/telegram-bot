574a2110f375544d903f2f340cb2a89dd9542508
vbncursed <enrbyltw@gmail.com>
2024-07-18 09:22:27 +0500

    Changes to be committed: 	modified:   bot/messages.py 	modified:   bot/requirements.txt

--- bot/messages.py
+++ bot/messages.py
@@ -6,6 +6,7 @@ CARD_NUMBER = getenv("CARD_NUMBER")
 CREATOR_ID = getenv("CREATOR_ID")
 CREATOR_TAG = getenv("CREATOR_TAG")
 EXCEPTION_IDS = getenv("EXCEPTION_IDS")
+USDT_WALLET_ADDRESS = getenv("USDT_WALLET_ADDRESS")
 
 
 def welcome_message(user_name) -> str:
@@ -13,10 +14,7 @@ def welcome_message(user_name) -> str:
 
 
 def vpn_info_message() -> str:
-    return (
-        "С VPN ты сможешь:\n1. Обходить географические ограничения и получать доступ к контенту из любой точки мира.\n2. Защищать свои данные от хакеров и наблюдения.\n3. Анонимно и безопасно пользоваться общественными Wi-Fi сетями.\nИ многое другое!"
-        ""
-    )
+    return "С VPN ты сможешь:\n1. Обходить географические ограничения и получать доступ к контенту из любой точки мира.\n2. Защищать свои данные от хакеров и наблюдения.\n3. Анонимно и безопасно пользоваться общественными Wi-Fi сетями.\nИ многое другое!"
 
 
 def go_buy_vpn_message() -> str:
@@ -29,38 +27,43 @@ def vpn_choose_period_message() -> str:
 
 def one_month_message(telegram_id: int) -> str:
     if str(telegram_id) in EXCEPTION_IDS:
-        return "450 рублей – стоимость подписки на один месяц."
+        return "300 рублей – стоимость подписки на один месяц."
     else:
         return "150 рублей – стоимость подписки на один месяц."
 
 
 def three_month_message(telegram_id: int) -> str:
     if str(telegram_id) in EXCEPTION_IDS:
-        return "1275 рублей – стомость подписки на три месяца."
+        return "855 рублей – стомость подписки на три месяца."
     else:
-        return "425 рублей – стомость подписки на три месяца."
+        return "435 рублей – стомость подписки на три месяца."
 
 
 def six_month_message(telegram_id: int) -> str:
     if str(telegram_id) in EXCEPTION_IDS:
-        return "2400 рублей – стоимость подписки на шесть месяцев."
+        return "1620 рублей – стоимость подписки на шесть месяцев."
     else:
-        return "800 рублей – стоимость подписки на шесть месяцев."
+        return "840 рублей – стоимость подписки на шесть месяцев."
 
 
 def twelve_month_message(telegram_id: int) -> str:
     if str(telegram_id) in EXCEPTION_IDS:
-        return "4500 рублей – стоимость подписки на один год."
+        return "3000 рублей – стоимость подписки на один год."
     else:
-        return "1500 рублей – стоимость подписки на один год."
+        return "1620 рублей – стоимость подписки на один год."
 
 
 def payment_page_message() -> str:
-    return f'Для оплаты подписки переведите деньги по номеру карты: Т-банк: <code>{CARD_NUMBER}</code> , Эдуард К.\n\nВ данный момент онлайн оплата еще не настроена, она находится в разработке, вы можете написать главному администратору бота {CREATOR_TAG}, прислать ему чек оплаты и он выдаст вам конфиг для подключения VPN. Также после того как нажмете кнопку "Оплатил" вас перенесет в чат с главным администратором'
+    return (
+        f"Для оплаты подписки переведите деньги по номеру карты:\nТ-банк: <code>{CARD_NUMBER}</code> , Эдуард К.\n"
+        f"Или переведите USDT (TRC20) на адрес: <code>{USDT_WALLET_ADDRESS}</code>\n\n"
+        "В данный момент онлайн оплата еще не настроена, она находится в разработке, вы можете написать главному администратору бота "
+        f"{CREATOR_TAG}, прислать ему чек оплаты и он выдаст вам конфиг для подключения VPN."
+    )
 
 
 def paid_message() -> str:
-    return "Вы успешно оплатили подписку, вы можете начать пользоваться VPN."
+    return "Вы успешно оплатили подписку, вы можете начать пользоваться VPN. На данный момент на этапе разработки"
 
 
 def author_contact() -> str:
